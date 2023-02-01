d980a2b2c7bf0ed537723d1c4569742fa85f8cb1
Kofi Gyasi <kaygyasi715@gmail.com>
2023-02-01 06:50:50 +0000

    complete salary data model

--- Practice.Models/Regression/SalaryData/SalaryDataModel.cs
+++ Practice.Models/Regression/SalaryData/SalaryDataModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data;
+using System.Numerics;
+using System.Security.AccessControl;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Practice.Models.Extensions;
@@ -8,18 +10,63 @@ namespace Practice.Models.Regression.SalaryData;
 public class SalaryDataModel
 {
     private readonly IDataView _data;
-    public SalaryDataModel()
+    private readonly IEnumerable<float> _predictions;
+    public SalaryDataModel(params float[] yearsOfExperiences)
     {
         var context = new MLContext();
 
-        var loader = context.Data.CreateTextLoader(new[]
-        {
-            new TextLoader.Column("YearsExperience", DataKind.Single, 0),
-            new TextLoader.Column("Salary", DataKind.Double, 1)
-        }, hasHeader: true, separatorChar: ',');
+        _data = context.Data.LoadFromTextFile<SalaryData>(DataPath.SalaryData,
+            hasHeader: true, separatorChar: ',');
+        
+        var pipeline = 
+            context.Transforms.Concatenate("Features", 
+                    nameof(SalaryDataInput.YearsExperience))
+            .Append(context.Regression
+            .Trainers
+            .Sdca(nameof(SalaryData.Salary)));
 
-        _data = loader.Load(DataPath.SalaryData);
+        var transformedData = pipeline
+            .Fit(_data);
+
+        var predictionEngine = context.Model
+            .CreatePredictionEngine<SalaryDataInput, SalaryDataOutput>(transformedData);
+        
+        // Evaluating the model
+        // var pred = transformedData.Transform(_data);
+        // var metrics = context.Regression.Evaluate(pred, nameof(SalaryData.Salary)
+        //     , nameof(SalaryData.Salary));
+        // Console.WriteLine("Mean Absolute Error: " + metrics.MeanAbsoluteError);
+        // Console.WriteLine("Mean Squared Error: " + metrics.MeanSquaredError);
+        // Console.WriteLine("Root Mean Squared Error: " + metrics.RootMeanSquaredError);
+        // Console.WriteLine("RSquared: " + metrics.RSquared);
+
+        _predictions = Enumerable.Empty<float>();
+
+        foreach (var yearsOfExperience in yearsOfExperiences)
+        {
+            _predictions = _predictions.Append(predictionEngine
+                .Predict(new SalaryDataInput(yearsOfExperience)).Salary);
+        }
     }
 
     public DataTable? GetData() => _data.ToDataTable();
+    public IEnumerable<float> GetPredictions() => _predictions;
+}
+
+public class SalaryData
+{
+    [LoadColumn(0)]
+    [ColumnName("YearsExperience")]
+    public float YearsOfExperience { get; set; }
+
+    [LoadColumn(1)]
+    public float Salary { get; set; }
+}
+
+public record SalaryDataInput(float YearsExperience);
+
+public class SalaryDataOutput
+{
+    [ColumnName("Score")]
+    public float Salary { get; set; }
 }
--- Practice.UI/Program.cs
+++ Practice.UI/Program.cs
@@ -2,7 +2,9 @@
 using Practice.Models.Data_Preprocessing;
 using Practice.Models.Data_Preprocessing.AutoPass;
 using Practice.Models.Extensions;
+using Practice.Models.Regression.SalaryData;
 
+/*
 var autoPassModel = new AutoPassModel();
 var table = autoPassModel.GetData().ToDataTable();
 
@@ -18,6 +20,32 @@ if (table?.Rows != null)
         Console.WriteLine();
     }
 }
+*/
+
+var yrs = new float[] { 6.4f, 2.6f, 8, 5.9f };
+var salaryDataModel = new SalaryDataModel(yrs);
+var predictions = salaryDataModel.GetPredictions().ToList();
+
+for (var i = 0; i < yrs.Length; i++)
+{
+    Console.WriteLine($"{yrs[i]} - {predictions[i]}");
+}
+
+// var table = salaryDataModel.GetData();
+//
+// if (table?.Rows != null)
+// {
+//     foreach (DataRow row in table.Rows)
+//     {
+//         foreach (DataColumn column in table.Columns)
+//         {
+//             Console.Write(row[column] + "\t");
+//         }
+//
+//         Console.WriteLine();
+//     }
+// }
+
 
 
 
